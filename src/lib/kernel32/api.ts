7e63d9fd1e2e3f3e4a4c59f0dbad1f4f76f65343
waiting <1661926154@qq.com>
2017-10-19 11:12:30 +0800

    Add GetProcessHeaps(), HeapFree() of kernel32

--- src/lib/kernel32/api.ts
+++ src/lib/kernel32/api.ts
@@ -8,6 +8,10 @@ export const fnDef: GT.Win32FnDef = {
     GetModuleHandleW: [D.HMODULE, [D.LPCTSTR]],    // retrive value from buf by ret.ref().readUInt32()
 
     GetModuleHandleExW: [D.BOOL, [D.DWORD, D.LPCTSTR, D.HMODULE]],     // flags, optional LPCTSTR name, ref hModule
+
+    GetProcessHeaps: [D.DWORD, [D.DWORD, D.PHANDLE]],
+
+    HeapFree: [D.BOOL, [D.HANDLE, D.DWORD, D.LPVOID]],
 };
 
 export interface Win32Fn {
@@ -16,4 +20,8 @@ export interface Win32Fn {
     GetModuleHandleW(lpModuleName: GT.LPCTSTR): GT.HMODULE;
 
     GetModuleHandleExW(dwFlags: GT.DWORD, lpModuleName: GT.LPCTSTR | null, phModule: GT.HMODULE): GT.BOOL;
+
+    GetProcessHeaps(NumberOfHeaps: GT.DWORD, ProcessHeaps: GT.PHANDLE): GT.DWORD;
+
+    HeapFree( hHeap: GT.HANDLE, dwFlags: GT.DWORD, lpMem: GT.LPVOID | null): GT.BOOL;
 }
