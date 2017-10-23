acda619e251f20c81c2f794b28600fd7b5ad2d7e
waiting <1661926154@qq.com>
2017-10-23 09:32:37 +0800

    Add conf.ts

---
+++ src/lib/conf.ts
@@ -0,0 +1,53 @@
+// dict of windef value
+
+export const windefSet = new Set([
+    'bool',
+    'bool*',
+
+    'byte',
+
+    'char',
+    'char*',
+
+    'float',
+    'float*',
+
+    'int',
+    'int8',
+    'int16',
+    'int32',
+    'int64' , 
+
+    'int*',
+    'int32*',
+    'int64*',
+
+    'uint',
+    'uint8',
+    'uint16', 
+    'uint32',
+    'uint64',
+
+    'uint*',
+    'uint8*',
+    'uint16*',
+    'uint32*',
+    'uint64*',
+
+    'int**',
+    'uint32**',
+    'uint64**',
+
+    'long',
+    'longlong',
+
+    'long*',
+    'longlong*',
+
+    'pointer',
+    'ushort',
+
+    'void',
+    'void*',
+]);
+
