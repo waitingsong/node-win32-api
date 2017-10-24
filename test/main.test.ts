59f17750c90967dd6586071a43608c0448ff4174
waiting <1661926154@qq.com>
2017-10-24 15:52:44 +0800

    Fix variable scope error by arrow function

--- test/main.test.ts
+++ test/main.test.ts
@@ -20,69 +20,50 @@ for (let key of fs.readdirSync(dllDir)) {
 }
 
 describe(filename, () => {
+    this.windefSet = windefSet;
+
     for (let dll of dlls) {
-        const apiName: string = dll.slice(0, 1).toUpperCase() + dll.slice(1);
+        const apiName: string = dll.slice(0, 1).toUpperCase() + dll.slice(1).toLowerCase(); // User32, Kernel32, ...
         const module = Win[apiName];
 
         if (module && module.api) {
-            this.apiDef = <GT.Win32FnDef> module.api;
+            const apiDef = <GT.Win32FnDef> module.api;
 
-            it(`Should ${apiName}: Win32FnName of definition be string`, () => {
-                const apiDef = <GT.Win32FnDef> this.apiDef;
-
-                if (apiDef) {
-                    for (let x in apiDef) {    // tslint:disable-line
-                        assert(typeof x === 'string');
-                    }
+            it(`Should ${apiName}: Win32FnName of definition be string`, function() {
+                for (let x in apiDef) {    // tslint:disable-line
+                    assert(typeof x === 'string');
                 }
             });
 
-            it(`Should ${apiName}: Win32FnParam of definition be array`, () => {
-                const apiDef = <GT.Win32FnDef> this.apiDef;
-
-                if (apiDef) {
-                    for (let x in apiDef) {    // tslint:disable-line
-                        const p = apiDef[x];
-                        assert(typeof p === 'object' && Array.isArray(p));
-                    }
+            it(`Should ${apiName}: Win32FnParam of definition be array`, function() {
+                for (let x in apiDef) {    // tslint:disable-line
+                    const p = apiDef[x];
+                    assert(typeof p === 'object' && Array.isArray(p));
                 }
             });
 
-            it(`Should ${apiName}: Win32FnRetType of definition be string`, () => {
-                const apiDef = <GT.Win32FnDef> this.apiDef;
-
-                if (apiDef) {
-                    for (let x in apiDef) {    // tslint:disable-line
-                        const p = apiDef[x];
-                        assert(typeof p[0] === 'string');
-                    }
+            it(`Should ${apiName}: Win32FnRetType of definition be string`, function() {
+                for (let x in apiDef) {    // tslint:disable-line
+                    const p = apiDef[x];
+                    assert(typeof p[0] === 'string');
                 }
             });
 
-            it(`Should ${apiName}: Win32FnCallParam of definition be array`, () => {
-                const apiDef = <GT.Win32FnDef> this.apiDef;
-
-                if (apiDef) {
-                    for (let x in apiDef) {    // tslint:disable-line
-                        const p = apiDef[x];
-                        assert(typeof p[1] === 'object' && Array.isArray(p[1]));
-                    }
+            it(`Should ${apiName}: Win32FnCallParam of definition be array`, function() {
+                for (let x in apiDef) {    // tslint:disable-line
+                    const p = apiDef[x];
+                    assert(typeof p[1] === 'object' && Array.isArray(p[1]));
                 }
             });
 
+            it(`Should ${apiName}: item of Win32FnCallParam of definition be string or void`, function() {
+                for (let x in apiDef) {    // tslint:disable-line
+                    const arr = apiDef[x][1];
+                    const len = arr.length;
 
-            it(`Should ${apiName}: item of Win32FnCallParam of definition be string or void`, () => {
-                const apiDef = <GT.Win32FnDef> this.apiDef;
-
-                if (apiDef) {
-                    for (let x in apiDef) {    // tslint:disable-line
-                        const arr = apiDef[x][1];
-                        const len = arr.length;
-
-                        if (len) {
-                            for (let i = 0; i < len; i++) {
-                                assert(typeof arr[i] === 'string' && arr[i]);
-                            }
+                    if (len) {
+                        for (let i = 0; i < len; i++) {
+                            assert(typeof arr[i] === 'string' && arr[i]);
                         }
                     }
                 }
@@ -90,8 +71,5 @@ describe(filename, () => {
 
         }
 
-    }
-
-
+    }   // loop END
 });
-
