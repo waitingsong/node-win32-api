eae1e8540f19f3ebf931aaa47d5c7d8149ee38eb
waiting <1661926154@qq.com>
2017-10-26 18:40:58 +0800

    Change value of DLL.api as converted value by helper.gen_api_opts(fnDef)

--- src/lib/comctl32/index.ts
+++ src/lib/comctl32/index.ts
@@ -2,11 +2,11 @@
 import {basename} from 'path';
 import * as GT from '../types';
 import * as W from '../windef';
-import {load as hload} from '../helper';
+import {gen_api_opts, load as hload} from '../helper';
 import {fnDef, Win32Fn} from './api';
 
 export {GT as types};
 export {W as windef};
-export {fnDef as api};
+export const api: GT.Win32FnDef = gen_api_opts(fnDef);
 export const dllName = basename(__dirname);
 export const load = (...args: any[]) => hload<Win32Fn>(dllName, fnDef, ...args);
