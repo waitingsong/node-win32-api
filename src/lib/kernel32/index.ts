a351827cb323156250b4832aac1813f2882d1d17
waiting <1661926154@qq.com>
2017-10-28 11:52:19 +0800

    Change output apidef of DLL as apiDef

--- src/lib/kernel32/index.ts
+++ src/lib/kernel32/index.ts
@@ -4,7 +4,7 @@ import * as GT from '../types';
 import {apiDef, Win32Fn} from './api';
 import {gen_api_opts, parse_windef, load as hload} from '../helper';
 
+export {apiDef};
 export {GT as types};
-export {apiDef as apidef};
 export const dllName = basename(__dirname);
 export const load = (...args: any[]) => hload<Win32Fn>(dllName, apiDef, ...args);
