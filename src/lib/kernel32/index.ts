d52f3c0d6862446363e6cef50d2954f79b427119
waiting <1661926154@qq.com>
2017-10-28 11:50:30 +0800

    Remove output windef as member of DLL like User32.windef, use top output instead

--- src/lib/kernel32/index.ts
+++ src/lib/kernel32/index.ts
@@ -1,12 +1,10 @@
 
 import {basename} from 'path';
 import * as GT from '../types';
-import * as W from '../windef';
 import {apiDef, Win32Fn} from './api';
 import {gen_api_opts, parse_windef, load as hload} from '../helper';
 
 export {GT as types};
 export {apiDef as apidef};
 export const dllName = basename(__dirname);
-export const windef: GT.Windef = parse_windef(W);
 export const load = (...args: any[]) => hload<Win32Fn>(dllName, apiDef, ...args);
