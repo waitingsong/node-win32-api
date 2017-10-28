755cf0f0922f666f1b15dd66203bca4f57e35eca
waiting <1661926154@qq.com>
2017-10-28 11:40:17 +0800

    Change name of DLL.api to DLL.apidef, with original not parsed definition

--- src/lib/user32/index.ts
+++ src/lib/user32/index.ts
@@ -6,7 +6,7 @@ import {apiDef, Win32Fn} from './api';
 import {gen_api_opts, parse_windef, load as hload} from '../helper';
 
 export {GT as types};
+export {apiDef as apidef};
 export const dllName = basename(__dirname);
 export const windef: GT.Windef = parse_windef(W);
-export const api: GT.ApiDef = gen_api_opts(apiDef);
 export const load = (...args: any[]) => hload<Win32Fn>(dllName, apiDef, ...args);
