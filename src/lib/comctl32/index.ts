9c3d8b3f9a429320b486aa12d915489b65798516
waiting <1661926154@qq.com>
2017-10-28 20:18:22 +0800

    types: export Win32Fns as DLL.Win32Fns

--- src/lib/comctl32/index.ts
+++ src/lib/comctl32/index.ts
@@ -5,5 +5,6 @@ import {apiDef, Win32Fns} from './api';
 import {gen_api_opts, parse_windef, load as hload} from '../helper';
 
 export {apiDef};
+export {Win32Fns};
 export const dllName = basename(__dirname);
 export const load = (fns?: GT.FnName[], settings?: GT.LoadSettings) => hload<Win32Fns>(dllName, apiDef, fns, settings);
