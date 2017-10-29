ce975458bd069a4712b11ca9aaacda9379ec95ba
waiting <1661926154@qq.com>
2017-10-29 19:16:29 +0800

    export User32.constand

--- src/lib/user32/index.ts
+++ src/lib/user32/index.ts
@@ -3,8 +3,10 @@ import {basename} from 'path';
 import * as GT from '../types';
 import {apiDef, Win32Fns} from './api';
 import {gen_api_opts, parse_windef, load as hload} from '../helper';
+import * as constant from './constant';
 
 export {apiDef};
+export {constant};
 export {Win32Fns};
 export const dllName = basename(__dirname);
 export const load = (fns?: GT.FnName[], settings?: GT.LoadSettings) => hload<Win32Fns>(dllName, apiDef, fns, settings);
