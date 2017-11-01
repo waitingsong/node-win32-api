5910e75d7ac57f17b03554bd0dec8003448e8979
waiting <1661926154@qq.com>
2017-11-01 09:37:07 +0800

    Rename constant.ts to constants.ts, and export as constants

--- src/lib/user32/index.ts
+++ src/lib/user32/index.ts
@@ -3,10 +3,10 @@ import {basename} from 'path';
 import * as GT from '../types';
 import {apiDef, Win32Fns} from './api';
 import {gen_api_opts, parse_windef, load as hload} from '../helper';
-import * as constant from './constant';
+import * as constants from './constants';
 
 export {apiDef};
-export {constant};
+export {constants};
 export {Win32Fns};
 export const dllName = basename(__dirname);
 export const load = (fns?: GT.FnName[], settings?: GT.LoadSettings) => hload<Win32Fns>(dllName, apiDef, fns, settings);
