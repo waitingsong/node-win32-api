2b737a2834196dfcb296ed1f5ff2b0de71071067
waiting <1661926154@qq.com>
2017-11-05 17:12:42 +0800

    test: increate wait time

--- test/70_find_calc.test.ts
+++ test/70_find_calc.test.ts
@@ -32,7 +32,7 @@ describe(filename, () => {
 
             child.kill();
             done();
-        }, 300);
+        }, 600);
     });
 
     it('Open a calc.exe and change it\'s window title', function(done) {
@@ -77,7 +77,7 @@ describe(filename, () => {
 
             child.kill();
             done();
-        }, 300);
+        }, 600);
     });
 
 });
