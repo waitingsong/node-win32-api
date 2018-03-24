073717e2d41939a48cbdf7072f57f28b152a43d1
waiting <1661926154@qq.com>
2018-03-24 19:38:27 +0800

    test: add 10_config.test.ts

---
+++ packages/win32-def/test/10_config.test.ts
@@ -0,0 +1,37 @@
+/// <reference types="mocha" />
+
+import * as assert from 'power-assert'
+
+import {
+  settingsDefault,
+  windefSet,
+  _UNICODE,
+  _WIN64,
+} from '../src/lib/config'
+import { basename } from '../src/shared/index'
+
+const filename = basename(__filename)
+
+describe(filename, () => {
+    it('Should items of windefSet must be typeof string and not empty', () => {
+        for (const vv of windefSet) {
+            assert(typeof vv === 'string', 'value must be string')
+            assert(vv.length, 'value must be not empty string')
+        }
+    })
+
+    it('Should value of _WIN64 correctly', () => {
+        const WIN64 = process.arch === 'x64' ? true : false
+
+        assert(WIN64 === _WIN64)
+    })
+
+    it('Should value of settingsDefault correctly', () => {
+        const st = settingsDefault
+
+        assert(st._UNICODE === _UNICODE)
+        assert(st._WIN64 === _WIN64)
+        assert(typeof st._windefClone === 'boolean' && st._windefClone === false)
+        assert(typeof st.singleton === 'boolean')
+    })
+})
