72091451cbf1d2be6ac48a97daa2c3b0ce248ae3
waiting <1661926154@qq.com>
2018-03-25 16:47:42 +0800

    test: add 15_windef.test.ts, 50_helper.test.ts

---
+++ packages/win32-def/test/15_windef.test.ts
@@ -0,0 +1,167 @@
+/// <reference types="mocha" />
+
+import * as assert from 'power-assert'
+import rewire = require('rewire')
+
+import { settingsDefault } from '../src/lib/config'
+import * as H from '../src/lib/helper'
+import * as WM from '../src/lib/model'
+import * as WD from '../src/lib/windef'
+import {
+  basename, logger,
+} from '../src/shared/index'
+
+const filename = basename(__filename)
+const mods = rewire('../src/lib/helper')
+
+describe(filename, () => {
+  const types64_32 = new Set([
+    'PVOID', 'HANDLE', 'HACCEL', 'HBITMAP',
+    'HBRUSH', 'HCOLORSPACE', 'HCONV', 'HCONVLIST',
+    'HCURSOR', 'HDC', 'HDDEDATA', 'HDESK',
+    'HDROP', 'HDWP', 'HENHMETAFILE', 'HFILE',
+    'HFONT', 'HGDIOBJ', 'HGLOBAL', 'HHOOK',
+    'HICON', 'HINSTANCE', 'HKEY', 'HKL',
+    'HLOCAL', 'HMENU', 'HMETAFILE', 'HMODULE',
+    'HMONITOR', 'HPALETTE', 'HPEN', 'HRGN',
+    'HRSRC', 'HSZ', 'HWINEVENTHOOK', 'HWINSTA',
+    'HWND', 'LPHANDLE', 'SC_HANDLE', 'SERVICE_STATUS_HANDLE',
+    'ULONG_PTR', 'DWORD_PTR', 'PDWORD_PTR', 'PSIZE_T', 'SIZE_T',
+    'POINTER_32', 'POINTER_64', 'PHKEY',
+  ])
+  const typesHalf = new Set([
+    'HALF_PTR', 'UHALF_PTR',
+  ])
+
+  test_arch(types64_32)
+  test_arch_half(typesHalf)
+})
+
+function test_arch(types64_32: Set<string>) {
+  const st = {...settingsDefault, _UNICODE: true, _WIN64: true}
+
+  st._WIN64 = false
+  _test_arch(types64_32, st)
+  // for (const k of Object.keys(st)) {
+  //   const opts = {...st}
+
+  //   if (st[k] === true) {
+  //     opts[k] = !st[k]
+  //   }
+  //   _test_arch(types64_32, opts)
+  // }
+  // for (const k of Object.keys(st)) {
+  //   const opts = {...st}
+  //   if (st[k] === false) {
+  //     opts[k] = !st[k]
+  //   }
+  //   _test_arch(types64_32, opts)
+  // }
+}
+
+function _test_arch(types64_32: Set<string>, settings: WM.LoadSettings) {
+  const W = H.parse_windef(WD, {...settings, _windefClone: true})
+
+  for (const vv of types64_32) {
+    // convert param like '_WIN64_HOLDER_' to 'int64' or 'int32'
+    const param = W[vv]
+
+    it(`Should ${vv}: value converted correctly under nodejs ${settings._WIN64 ? 'x64' : 'ia32'}`, () => {
+      if (settings._WIN64) {
+        // must use param not W[vv]
+        assert(param.indexOf('64') > 2 && param.indexOf('32') === -1, `"${vv}: ${param}" invalid during x64`)
+      }
+      else {
+        if (!(param.indexOf('32') > 2 && param.indexOf('64') === -1)) {
+          logger('er:::', W)
+        }
+        assert(param.indexOf('32') > 2 && param.indexOf('64') === -1, `"${vv}: ${param}" invalid during ia32`)
+      }
+    })
+  }
+}
+
+function test_arch_half(values: Set<string>) {
+  const st = {...settingsDefault, _UNICODE: true, _WIN64: true}
+
+  for (const k of Object.keys(st)) {
+    if (st[k]) {
+      _test_arch_half(values, {...st, [k]: !st[k]})
+    }
+  }
+  for (const k of Object.keys(st)) {
+    if (!st[k]) {
+      _test_arch_half(values, {...st, [k]: !st[k]})
+    }
+  }
+}
+
+function _test_arch_half(typesHalf: Set<string>, settings: WM.LoadSettings) {
+  const W = H.parse_windef(WD, {...settings, _windefClone: true})
+  const fnName = 'parse_param_placeholder'
+  const fn = <(
+    param: WM.FFIParam | WM.MacroDef,
+    settings?: WM.LoadSettings
+  ) => WM.FFIParam> mods.__get__(fnName)
+
+
+  for (const vv of typesHalf) {
+    // convert param like ['_WIN64_HOLDER_', 'int64', 'int32'] to 'int64' or 'int32'
+    const param = fn(W[vv], settings)
+
+    it(`Should ${vv}: value converted correctly under nodejs ${settings._WIN64 ? 'x64' : 'ia32'}`, () => {
+      if (settings._WIN64) {
+        const cond: boolean = !! param && typeof param === 'string' &&
+          param.indexOf('32') > 2 &&
+          param.indexOf('16') === -1 &&
+          param.indexOf('64') === -1
+
+        assert(cond, `${vv}: ${param} under x64`)   // must use param not W[vv]
+      }
+      else {
+        const cond: boolean = !! param && typeof param === 'string' &&
+          param.indexOf('16') > 2 &&
+          param.indexOf('32') === -1 &&
+          param.indexOf('64') === -1
+
+        assert(cond, `${vv}: ${param} under ia32`)
+      }
+    })
+  }
+}
+
+describe(filename, () => {
+  const typesUnicode = new Set([
+    'LPCTSTR', 'LPTSTR', 'PTBYTE', 'PTCHAR',
+    'PTSTR', 'TBYTE', 'TCHAR',
+  ])
+
+  unicode(true, typesUnicode)
+  unicode(false, typesUnicode)
+})
+
+function unicode(_UNICODE: boolean, typesUnicode: Set<string>) {
+  const W = H.parse_windef(WD, {...settingsDefault, _UNICODE, _windefClone: true})
+
+  for (const vv of typesUnicode) {
+    const param = W[vv]
+
+    it(`Should macro ${vv}: value mathes setting of ANSI/UNICODE`, () => {
+      if (_UNICODE) {
+        const cond: boolean = !! param && typeof param === 'string' &&
+          param.indexOf('16') > 2 &&
+          param.indexOf('8') === -1
+
+        assert(cond, `${vv}: ${param} at UNICODE`)
+      }
+      else {
+        // PTSTR == 'char*' under ia32
+        const cond: boolean = !! param && typeof param === 'string' &&
+          (param.indexOf('8') > 2 || param === 'char*') &&
+          param.indexOf('16') === -1
+
+        assert(cond, `${vv}: ${param} at ANSI`)
+      }
+    })
+  }
+}
---
+++ packages/win32-def/test/50_helper.test.ts
@@ -0,0 +1,289 @@
+/// <reference types="mocha" />
+
+import * as assert from 'power-assert'
+import rewire = require('rewire')
+
+import {
+  settingsDefault,
+  windefSkipKeys,
+  _UNICODE_HOLDER,
+  _WIN64_HOLDER,
+} from '../src/lib/config'
+import * as H from '../src/lib/helper'
+import * as WM from '../src/lib/model'
+import * as WD from '../src/lib/windef'
+import {
+  basename,
+  logger,
+} from '../src/shared/index'
+
+
+const filename = basename(__filename)
+const mods = rewire('../src/lib/helper')
+
+
+describe(filename + ' :parse_param_placeholder(param, settings?) ', () => {
+  const fnName = 'parse_param_placeholder'
+  const fn = <(
+    param: WM.FFIParam | WM.MacroDef,
+    settings?: WM.LoadSettings
+  ) => WM.FFIParam> mods.__get__(fnName)
+
+  it(`Should ${fnName} handle value of settings correctly)`, () => {
+    const st = <WM.LoadSettings> {...settingsDefault}
+    try {
+      const p: any = null
+      fn(p, st)
+      assert(false, 'should throw Error by invalid param, but not')
+    }
+    catch (ex) {
+      assert(true)
+    }
+  })
+
+  it(`Should ${fnName} handle value of param correctly)`, () => {
+    const st = <WM.LoadSettings> {...settingsDefault}
+    try {
+      const p: WM.MacroDef = ['invalid_placeholder', 'int64', 'int32']
+      fn(p, st)
+      assert(false, 'should throw Error by invalid param, but not')
+    }
+    catch (ex) {
+      assert(true)
+    }
+  })
+
+  it(`Should ${fnName} handle value of settings for arch of nodejs correctly)`, () => {
+    const p1 = 'debug_int64'
+    const p2 = 'debug_int32'
+    const p: WM.MacroDef = [_WIN64_HOLDER, p1, p2]
+    const st = {...settingsDefault}
+    const str1 = fn(p, {...st, _WIN64: true})
+    assert(str1 === p1, `result should be "${p1}", got ${str1}`)
+
+    const str2 = fn(p, {...st, _WIN64: false})
+    assert(str2 === p2, `result should be "${p2}", got ${str2}`)
+  })
+
+  it(`Should ${fnName} handle value of settings for ANSI/UNICODE correctly)`, () => {
+    const LPTSTR: WM.MacroDef = [_UNICODE_HOLDER, WD.LPWSTR, 'uint8*']
+    const st = {...settingsDefault}
+    const str1 = fn(LPTSTR, {...st, _UNICODE: true})
+    assert(str1 === LPTSTR[1], `result should be "${LPTSTR[1]}", got ${str1}`)
+
+    const str2 = fn(LPTSTR, {...st, _UNICODE: false})
+    assert(str2 === LPTSTR[2], `result should be "${LPTSTR[2]}", got ${str2}`)
+  })
+
+})
+
+
+describe(filename + ' :parse_placeholder_arch(param, _WIN64)', () => {
+  const fnName = 'parse_placeholder_arch'
+  const fn = <(
+    param: WM.FFIParam | WM.MacroDef,
+    _WIN64: boolean
+  ) => WM.FFIParam> mods.__get__(fnName)
+
+  it(`Should ${fnName} handle value of param correctly)`, () => {
+    const p: any = 'test'
+    const res = fn(p, true)
+    assert(res === p, `should ${p} got ${res}`)
+  })
+
+  it(`Should ${fnName} handle value of param correctly)`, () => {
+    try {
+      const p: any = null
+
+      fn(p, true)
+      assert(false, 'should throw Error by invalid param, but not')
+    }
+    catch (ex) {
+      assert(true)
+    }
+  })
+
+  it(`Should ${fnName} handle value of param correctly)`, () => {
+    try {
+      const p: any = [1, 2]    // should 3 items
+      fn(p, true)
+      assert(false, 'should throw Error by invalid param, but not')
+    }
+    catch (ex) {
+      assert(true)
+    }
+  })
+
+})
+
+describe(filename + ' :parse_placeholder_unicode(param, _WIN64)', () => {
+  const fnName = 'parse_placeholder_unicode'
+  const fn = <(
+    param: WM.FFIParam | WM.MacroDef,
+    _UNICODE: boolean
+  ) => WM.FFIParam> mods.__get__(fnName)
+
+  it(`Should ${fnName} handle value of param correctly)`, () => {
+    const p: any = 'test'
+    const res = fn(p, true)
+    assert(res === p, `should ${p} got ${res}`)
+  })
+
+  it(`Should ${fnName} handle value of param correctly)`, () => {
+    try {
+      const p: any = null
+
+      fn(p, true)
+      assert(false, 'should throw Error by invalid param, but not')
+    }
+    catch (ex) {
+      assert(true)
+    }
+  })
+
+  it(`Should ${fnName} handle value of param correctly)`, () => {
+    try {
+      const p: any = [1, 2]    // should 3 items
+      fn(p, true)
+      assert(false, 'should throw Error by invalid param, but not')
+    }
+    catch (ex) {
+      assert(true)
+    }
+  })
+
+})
+
+describe(filename + ' :parse_windef()', () => {
+  const fnName = 'parse_windef()'
+  const fake = 'fake'
+
+  it(`Should ${fnName} process windef with fake windef correctly)`, () => {
+    const W = {...WD}
+
+    Object.defineProperty(W, fake, {
+      configurable: true,
+      writable: true,
+      enumerable: true,
+      value: 777, // should string or string[]
+    })
+    try {
+      H.parse_windef(W)
+      assert(false, 'should throw Error, but none')
+    }
+    catch (ex) {
+      assert(true)
+    }
+
+    Object.getOwnPropertyNames(W).forEach((val, idx) => {
+      if (val === fake) {
+        W[val] = 'int'
+      }
+    })
+    Object.defineProperty(W, 777, { // should string
+      configurable: true,
+      writable: true,
+      enumerable: true,
+      value: 'int',
+    })
+    try {
+      H.parse_windef(W)
+      assert(false, 'should throw Error, but none')
+    }
+    catch (ex) {
+      Object.defineProperty(W, 777, { // should string
+        enumerable: false,
+      })
+      assert(true)
+    }
+  })
+
+  it(`Should ${fnName} process windef macro members correctly)`, () => {
+    const W = <WM.Windef> {}
+    const keyArch = '__testKeyArch'
+    const v64 = '_v64'
+    const v32 = '_v32'
+
+    W[keyArch] = _WIN64_HOLDER
+    W.macroMap = <WM.MacroMap> new Map([
+      [keyArch, [_WIN64_HOLDER, v64, v32]],
+    ])
+
+    let _WIN64 = true
+    try {
+      H.parse_windef(W, {...settingsDefault, _windefClone: true, _WIN64})
+      assert(false, 'should throw error by validDataDef() BUT not')
+    }
+    catch (ex) {
+      assert(true)
+    }
+
+    _WIN64 = false
+    try {
+      H.parse_windef(W, {...settingsDefault, _windefClone: true, _WIN64})
+      assert(false, 'should throw error by validDataDef() BUT not')
+    }
+    catch (ex) {
+      assert(true)
+    }
+
+    const keyUni = '__testKeyUNI'
+    const uni = '_valueUNICODE'
+    const ansi = '_valueANSI'
+
+    delete W[keyArch]
+    W[keyUni] = _UNICODE_HOLDER
+    W.macroMap = <WM.MacroMap> new Map([
+      [keyUni, [_UNICODE_HOLDER, uni, ansi]],
+    ])
+
+    let _UNICODE = true
+    try {
+      H.parse_windef(W, {...settingsDefault, _windefClone: true, _UNICODE})
+      assert(false, 'should throw error by validDataDef() BUT not')
+    }
+    catch (ex) {
+      assert(true)
+    }
+
+    _UNICODE = false
+    try {
+      H.parse_windef(W, {...settingsDefault, _windefClone: true, _UNICODE})
+      assert(false, 'should throw error by validDataDef() BUT not')
+    }
+    catch (ex) {
+      assert(true)
+    }
+
+  })
+
+  // at lastest
+  it(`Should ${fnName} process windef correctly)`, () => {
+    const W = {...WD}
+    const windata = H.parse_windef(W, {...settingsDefault, _windefClone: true})
+    const lenData = Object.keys(windata).length + windefSkipKeys.size
+    const lenDef = Object.keys(W).length
+
+    if (lenData !== lenDef) {
+      const onlyInRet = <Set<string>> new Set()
+      const onlyInW = <Set<string>> new Set()
+
+      for (const key of Object.keys(windata)) {
+        if (typeof W[key] === 'undefined') {
+          onlyInRet.add(key)
+        }
+      }
+      for (const key of Object.keys(W)) {
+        if (typeof windata[key] === 'undefined' && ! windefSkipKeys.has(key)) {
+          onlyInW.add(key)
+        }
+      }
+
+      logger(onlyInRet, onlyInW)
+      assert(false, `lenData:${lenData}, lenDef:${lenDef} not equal `)
+    }
+
+  })
+
+})
+
