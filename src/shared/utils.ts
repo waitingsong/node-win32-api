371a31358bd6375929db935abf74e9637f09adb3
waiting <1661926154@qq.com>
2018-04-11 20:58:56 +0800

    fix: path require parse by normalize() within createDir()

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -76,6 +76,7 @@ export async function createDir(path: string): Promise<void> {
     throw new Error('value of path param invalid')
   }
   else {
+    path = normalize(path)  // ! required
     /* istanbul ignore else */
     if (!await isDirExists(path)) {
       await path.split(sep).reduce(
