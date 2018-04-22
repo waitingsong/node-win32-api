5f4a5bcbafc850b08970cc5d610bb5b06abe7da8
waiting <1661926154@qq.com>
2018-04-22 19:47:00 +0800

    chore: update configurations for rollup, tsc default ES6
    change default ts compile module to 'ES6'

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,45 @@
+import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
+import uglify from 'rollup-plugin-uglify'
+import pkg from './package.json';
 
+// `npm run build` -> `production` is true
+// `npm run dev` -> `production` is false
+const production = !process.env.ROLLUP_WATCH;
 
-export default {
-  input: 'dist/index.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'amd'
+const config = [
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  {
+    external: ['rxjs', 'rxjs/operators', 'fs', 'path', 'util', 'os'],
+      input: pkg.module,
+      output: [
+        { file: pkg.main, format: 'cjs' },
+      ],
   },
-  plugins: [ resolve() ]
+]
+
+// browser-friendly UMD build
+if (pkg.browser ) {
+  config.push({
+      input: pkg.module,
+      plugins: [
+        resolve({
+          browser: true,
+          jsnext: true,
+          main: true,
+        }),
+        commonjs(),
+        production && uglify(),
+      ],
+      output: {
+        amd: { id: pkg.name },
+        file: pkg.browser,
+        format: 'umd',
+        name: pkg.name,
+        sourcemap: true,
+      },
+  })
 }
+
+export default config
+
