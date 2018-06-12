4b8bae61e6588f4b7b776796513cb3cd48ba501d
waiting <1661926154@qq.com>
2018-06-12 16:42:55 +0800

    chore: update test code

--- test/70_find_calc.test.ts
+++ test/70_find_calc.test.ts
@@ -14,7 +14,7 @@ const knl32 = K.load()
 const user32 = U.load()
 
 describe(filename, () => {
-  it('Open a calc.exe and find it\'s window hWnd', function(done) {
+  it('Open a calc.exe and find it\'s window hWnd', done => {
     const child = spawn('calc.exe')
 
     setTimeout(() => {
@@ -33,7 +33,7 @@ describe(filename, () => {
     }, 3000)
   })
 
-  it('Open a calc.exe and change it\'s window title', function(done) {
+  it('Open a calc.exe and change it\'s window title', done => {
     const child = spawn('calc.exe')
 
     setTimeout(() => {
@@ -50,6 +50,7 @@ describe(filename, () => {
           const errcode = knl32.GetLastError()
           const len = 255
           const buf = Buffer.alloc(len)
+          // tslint:disable-next-line
           const p = 0x00001000 | 0x00000200  // FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS
           const langid = 0x0409              // 0x0409: US, 0x0000: Neutral locale language
           const msglen = knl32.FormatMessageW(p, null, errcode, langid, buf, len, null)
