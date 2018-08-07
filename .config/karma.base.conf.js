d692bb30bf4a47e6668a0c95fb21a8c87e04977a
waiting <1661926154@qq.com>
2018-08-07 19:21:36 +0800

    test(karma): update config for es2016

--- .config/karma.base.conf.js
+++ .config/karma.base.conf.js
@@ -7,10 +7,11 @@ module.exports = {
   ],
 
   files: [
+    { pattern: '.config/node_modules/es6-shim/es6-shim.min.js', watched: false },
+    { pattern: '.config/node_modules/es7-shim/dist/es7-shim.min.js', watched: false },
+    { pattern: '.config/node_modules/whatwg-fetch/fetch.js', watched: false },
     'src/**/*.ts',
     'test_browser/**/*.ts',
-    '.config/node_modules/es6-shim/es6-shim.min.js',
-    '.config/node_modules/whatwg-fetch/fetch.js',
   ],
 
   exclude: [
