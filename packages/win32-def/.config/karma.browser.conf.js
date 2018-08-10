09e930658b7252bffd8fa78f1c52ef5de431707f
waiting <1661926154@qq.com>
2018-08-10 21:04:37 +0800

    test: add entry of Karma Chrome for debugging in chrome

--- packages/win32-def/.config/karma.browser.conf.js
+++ packages/win32-def/.config/karma.browser.conf.js
@@ -1,10 +1,16 @@
 const base = require('./karma.base.conf.js')
 
 module.exports = (config) => {
+  if (config.mochaTimeout) {
+    base.client.mocha.timeout = config.mochaTimeout
+  }
+
   config.set(Object.assign(base, {
-    frameworks: base.frameworks.concat([
-      'detectBrowsers',
-    ]),
+    frameworks: ! config.customDebug
+      ?  base.frameworks.concat([
+          'detectBrowsers',
+        ])
+      : base.frameworks,
 
     detectBrowsers: {
       // enable/disable phantomjs support, default is true
@@ -19,9 +25,17 @@ module.exports = (config) => {
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_ERROR,
 
+    customLaunchers: {
+      ChromeDebugging: {
+        base: 'Chrome',
+        flags: [ '--remote-debugging-port=9333' ]
+      }
+    },
+
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     // browsers: ['Chrome', 'Firefox', 'IE'],
+    // browsers: ['ChromeDebugging'],
 
   }))
 }
