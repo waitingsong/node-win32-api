91da144c1614a34a3483b7eacfa4787751b79b38
waiting <1661926154@qq.com>
2018-08-23 16:38:34 +0800

    feat: add assertNeverObb()

--- src/lib/shared.ts
+++ src/lib/shared.ts
@@ -1,3 +1,4 @@
+import { throwError, Observable } from 'rxjs'
 
 /* istanbul ignore next */
 export function log(...args: any[]) {
@@ -9,4 +10,9 @@ export function log(...args: any[]) {
 /* istanbul ignore next */
 export function assertNever(x: never): never {
   throw new Error('Assert Never Unexpected object: ' + x)
+}
+
+/* istanbul ignore next */
+export function assertNeverObb(x: never): Observable<never> {
+  return throwError(new Error('Assert Never Unexpected object: ' + x))
 }
