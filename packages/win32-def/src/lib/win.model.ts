d765e39742e2cbd2e7e481e2f85485b7cbd54cb0
waiting <1661926154@qq.com>
2018-09-17 09:30:55 +0800

    style: format

--- packages/win32-def/src/lib/win.model.ts
+++ packages/win32-def/src/lib/win.model.ts
@@ -250,6 +250,7 @@ export interface RectStruct {
   right: LONG
   bottom: LONG
 }
+
 export const enum EXECUTION_STATE {
   /**
    * Enables away mode. This value must be specified with ES_CONTINUOUS.
@@ -257,14 +258,15 @@ export const enum EXECUTION_STATE {
    * applications that must perform critical background processing on desktop
    * computers while the computer appears to be sleeping. See Remarks.
    */
-  ES_CONTINUOUS        = 0x80000000,
+  ES_CONTINUOUS = 0x80000000,
   /**
    * Informs the system that the state being set should remain in effect until
    * the next call that uses ES_CONTINUOUS and one of the other state flags is cleared.
    */
   ES_AWAYMODE_REQUIRED = 0x00000040,
   /** Forces the display to be on by resetting the display idle timer. */
-  ES_SYSTEM_REQUIRED   = 0x00000001,
+  ES_SYSTEM_REQUIRED = 0x00000001,
   /** Forces the system to be in the working state by resetting the system idle timer. */
-  ES_DISPLAY_REQUIRED  = 0x00000002
+  ES_DISPLAY_REQUIRED = 0x00000002,
 }
+
