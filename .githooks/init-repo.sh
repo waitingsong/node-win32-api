dd8974784559f442c297419eedb84a12debf0723
waiting <1661926154@qq.com>
2019-01-21 11:41:11 +0800

    chore(scripts): update repo:init

---
+++ .githooks/init-repo.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# 初始化 git 仓库
+#
+# Author: waiting
+# Date: 2019.01.21
+#
+
+git config --local push.followTags true \
+  && git config --local core.hooksPath ./.githooks \
+  && git config --local remote.origin.prune true \
+  && git config --local remote.origin.tagopt --tags \
+  && git config --local remote.pushdefault origin \
+  && echo It may going for a long time. Plese wait... \
+  && npm i \
+  && cd .githooks && tsc \
+  && node gen-file-from-example.js
+
