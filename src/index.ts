988d4b087aad386cf401519abf069afc601d7798
waiting <1661926154@qq.com>
2019-02-21 16:35:44 +0800

    chore: format

--- src/index.ts
+++ src/index.ts
@@ -13,19 +13,20 @@ export {
   FModel, // ffi model
 } from 'win32-def'
 
+import * as Comctl32 from './lib/comctl32/index'
+import * as Kernel32 from './lib/kernel32/index'
+import * as Ntdll from './lib/ntdll/index'
 import * as User32 from './lib/user32/index'
+
 export { User32 as U }
 export { User32 }
 
-import * as Kernel32 from './lib/kernel32/index'
 export { Kernel32 as K }
 export { Kernel32 }
 
-import * as Comctl32 from './lib/comctl32/index'
 export { Comctl32 as C }
 export { Comctl32 }
 
-import * as Ntdll from './lib/ntdll/index'
 export { Ntdll }
 
 export * from './lib/model'
