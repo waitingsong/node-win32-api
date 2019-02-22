bb3cd4522e5923d08bb0dec167b4e5f351b7f8da
waiting <1661926154@qq.com>
2019-02-22 21:00:40 +0800

    chore: update comments of FFIBuffer

--- packages/win32-def/src/lib/ffi.model.ts
+++ packages/win32-def/src/lib/ffi.model.ts
@@ -30,28 +30,52 @@ export interface LoadSettings {
  * with it hWnd.ref() wihout error TS2339: Property 'ref' does not exist on type 'Buffer'.
  */
 export interface FFIBuffer extends Buffer {
+  /** Shorthand for `ref.address`. */
   address(): number
+  /** Shorthand for `ref.deref`. */
   deref(): any
+  /** Shorthand for `ref.isNull`. */
   isNull(): boolean
+  /** Shorthand for `ref.readCString`. */
   readCString(offset?: number): string
+  /** Shorthand for `ref.readInt64BE`. */
   readInt64BE(offset?: number): string
+  /** Shorthand for `ref.readInt64LE`. */
   readInt64LE(offset?: number): string
+  /** Shorthand for `ref.readObject`. */
   readObject(offset?: number): string
+  /** Shorthand for `ref.readPointer`. */
   readPointer(offset?: number): string
+  /** Shorthand for `ref.readUInt64BE`. */
   readUInt64BE(offset?: number): string
+  /** Shorthand for `ref.readUInt64LE`. */
   readUInt64LE(offset?: number): string
+  /** Shorthand for `ref.ref`. */
   ref(): FFIBuffer
+  /** Shorthand for `ref.reinterpret`. */
   reinterpret(size: number, offset?: number): FFIBuffer
+  /** Shorthand for `ref.reinterpretUntilZeros`. */
   reinterpretUntilZeros(size: number, offset?: number): FFIBuffer
+  /** Shorthand for `ref.writeCString`. */
   writeCString(offset: number, input: string, encoding?: string): void
+  /** Shorthand for `ref.writeInt64BE`. */
   writeInt64BE(offset: number, input: number | string): any
+  /** Shorthand for `ref.writeInt64LE`. */
   writeInt64LE(offset: number, input: number | string): any
+  /** Shorthand for `ref.writeObject`. */
   writeObject(offset: number, object: Object): void;  // tslint:disable-line
+  /** Shorthand for `ref.writePointer`. */
   writePointer(offset: number, pointer: FFIBuffer): void
+  /** Shorthand for `ref.writeUInt64BE`. */
   writeUInt64BE(offset: number, input: number | string): any
+  /** Shorthand for `ref.writeUInt64LE`. */
   writeUInt64LE(offset: number, input: number | string): any
+  /**
+   * Generate string for inspecting.
+   * String includes the hex-encoded memory address of the Buffer instance.
+   */
   inspect(): string
-  // add by waiting. below extened via Buffer.prototype by ref.js
+  /** add by waiting. below extened via Buffer.prototype by ref.js */
   hexAddress(): string
 }
 
