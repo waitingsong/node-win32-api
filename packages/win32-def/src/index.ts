bf096a54916bd676f10d0d320baa577de3927586
waiting <1661926154@qq.com>
2019-02-24 17:53:23 +0800

    refactor: rename struct.ts to data-struct.ts

--- packages/win32-def/src/index.ts
+++ packages/win32-def/src/index.ts
@@ -7,10 +7,10 @@
  */
 
 import * as Config from './lib/config'
+import * as DStruct from './lib/data-struct'
 import * as FModel from './lib/ffi.model'
 import { parse_windef } from './lib/helper'
 import { macroMap } from './lib/marcomap'
-import * as DStruct from './lib/struct'
 import * as DModel from './lib/win.model'
 import * as windef from './lib/windef'
 
@@ -19,5 +19,5 @@ const DTypes = <typeof windef> parse_windef(windef, macroMap)
 export { Config }
 export { FModel } // ffi model
 export { DModel } // model of window data types
-export { DStruct }  // window constans data types of structure for ref-struct
+export { DStruct }  // window constans data types of structure for ref-struc
 export { DTypes } // window data types
