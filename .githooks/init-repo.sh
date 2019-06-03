64523d4812418bec9883eba21b2dd895a2c40da2
waiting <1661926154@qq.com>
2019-06-03 16:03:43 +0800

    chore: update init-repo.sh for git submodule

--- .githooks/init-repo.sh
+++ .githooks/init-repo.sh
@@ -8,9 +8,14 @@
 
 git config --local push.followTags true \
   && git config --local core.hooksPath ./.githooks \
+  && git config --local fetch.prune true \
+  && git config --local push.recurseSubmodules check \
   && git config --local remote.origin.prune true \
   && git config --local remote.origin.tagopt --tags \
   && git config --local remote.pushdefault origin \
+  && git config --local submodule.fetchJobs 2 \
+  && git config --local submodule.recurse true \
+  && git submodule update --init --recursive \
   && echo It may going for a long time. Plese wait... \
   && npm i --loglevel=info \
   && npm run link_global \
