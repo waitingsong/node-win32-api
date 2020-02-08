cd49d7d04d216b6b1826449d47531b2a52cc0e86
waiting <1661926154@qq.com>
2020-02-09 00:09:37 +0800

    test(win32-api): update 60_GetRawInputDeviceList.test.ts
    format style

--- packages/win32-api/test/user32/60_GetRawInputDeviceList.test.ts
+++ packages/win32-api/test/user32/60_GetRawInputDeviceList.test.ts
@@ -17,16 +17,18 @@ const filename = basename(__filename)
 
 describe(filename, () => {
   it('GetRawInputDeviceList()', () => {
-    const pRawInputDeviceList: M.RAWINPUTDEVICELIST_Struct = new Struct(DS.RAWINPUTDEVICELIST)()
     const limit = 255
+    const rawInputDeviceList: M.RAWINPUTDEVICELIST_Struct = new Struct(DS.RAWINPUTDEVICELIST)()
+    const pRawInputDeviceList = Buffer.alloc(rawInputDeviceList.ref().byteLength * limit)
     const puiNumDevices = Buffer.alloc(4)
     puiNumDevices[0] = limit
-    const buf = Buffer.alloc(pRawInputDeviceList.ref().byteLength * limit)
+
     const nDevices = user32.GetRawInputDeviceList(
-      buf,
+      pRawInputDeviceList,
       puiNumDevices,
-      pRawInputDeviceList.ref().byteLength,
+      rawInputDeviceList.ref().byteLength,
     )
+
     // console.log(buf)
     console.log({ nDevices, puiNumDevices })
     assert(nDevices > 0 && nDevices <= limit)
