8d585ba6dc4ffe78f02c020e1538ae71f5e0b166
waiting <1661926154@qq.com>
2020-03-03 14:55:32 +0800

    build(deps-dev): update boilerplate

--- packages/demo/rollup.config.js
+++ packages/demo/rollup.config.js
@@ -1,6 +1,6 @@
 import { dirname } from 'path'
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
