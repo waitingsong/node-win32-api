9e6abec425227484b46dd7a16788ae2c100624c8
waiting <1661926154@qq.com>
2021-04-15 11:46:19 +0800

    chore(boilerplate): update git config

--- .githooks/init-repo.sh
+++ .githooks/init-repo.sh
@@ -8,9 +8,14 @@
 
 git init \
   && git config --local i18n.commitencoding utf-8 \
-  && git config --local push.followTags true \
+  && git config --local core.autocrlf false \
+  && git config --local core.eol lf \
   && git config --local core.hooksPath ./.githooks \
+  && git config --local core.ignorecase false \
+  && git config --local core.precomposeUnicode true \
   && git config --local fetch.prune true \
+  && git config --local pull.rebase true \
+  && git config --local push.followTags true \
   && git config --local remote.origin.prune true \
   && git config --local remote.origin.tagopt --tags \
   && git config --local remote.pushdefault origin \
