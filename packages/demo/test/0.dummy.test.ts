df98b29088a91699587b04d67db0d526dc069058
waiting <1661926154@qq.com>
2021-11-19 16:02:19 +0800

    test(boilerplate): update demo

--- packages/demo/test/0.dummy.test.ts
+++ packages/demo/test/0.dummy.test.ts
@@ -1,16 +1,14 @@
-import {
-  basename,
-  join,
-} from '@waiting/shared-core'
+import { relative } from 'path'
+
 // eslint-disable-next-line import/order
 import assert = require('power-assert')
 
 
-const filename = basename(__filename)
+const filename = relative(process.cwd(), __filename).replace(/\\/ug, '/')
 
 describe(filename, () => {
 
-  describe('should works', () => {
+  describe('should work', () => {
     it('always passed', () => {
       assert(true)
     })
