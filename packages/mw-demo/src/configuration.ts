7bc3737b33adcf57194c33c6dc1427d3b8b1cdfe
waiting <1661926154@qq.com>
2022-03-10 21:04:16 +0800

    chore(boilerplate): update types

--- packages/mw-demo/src/configuration.ts
+++ packages/mw-demo/src/configuration.ts
@@ -4,10 +4,10 @@ import { join } from 'path'
 
 import { App, Config, Configuration } from '@midwayjs/decorator'
 
-import { ConfigKey } from './lib/config'
 import { DemoMiddleware } from './middleware/demo.middleware'
 
-import { Application, MiddlewareConfig, IMidwayContainer } from '~/interface'
+import { ConfigKey, MiddlewareConfig } from '~/index'
+import { Application, IMidwayContainer } from '~/interface'
 
 
 @Configuration({
--- packages/mw-demo/src/interface.ts
+++ packages/mw-demo/src/interface.ts
@@ -12,7 +12,6 @@ export {
   JsonObject,
   JsonResp,
   JsonType,
-  MiddlewareConfig,
   NpmPkg,
 } from '@waiting/shared-types'
 
