e42fbb52cb7841a2846770ca4c8ea5aa0107e469
waiting <1661926154@qq.com>
2022-05-30 15:14:56 +0800

    chore(boilerplate): update mw-demo/test/root.config.ts

--- packages/mw-demo/test/root.config.ts
+++ packages/mw-demo/test/root.config.ts
@@ -10,6 +10,7 @@ import {
 } from '~/lib/types'
 
 
+const CI = !! process.env.CI
 export type TestResponse = supertest.Response
 export interface TestRespBody {
   header: IncomingHttpHeaders
@@ -20,12 +21,14 @@ export interface TestRespBody {
 }
 
 export interface TestConfig {
+  CI: boolean
   app: Application
   config: Config
   host: string
   httpRequest: SuperTest<supertest.Test>
 }
 export const testConfig = {
+  CI,
   config,
   host: '',
 } as TestConfig
