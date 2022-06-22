052a02bd193aeb90ddafcda6c74e40adf4ab872c
waiting <1661926154@qq.com>
2022-06-22 15:59:45 +0800

    chore: remove mw-demo

--- packages/mw-demo/src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-
-export * from './config'
-export * from './types'
-export { DemoComponent } from './component-singleton'
-export { Demo2Component } from './component-request'
-
--- packages/mw-demo/test/fixtures/base-app/src/configuration.ts
+++
@@ -1,9 +0,0 @@
-import 'tsconfig-paths/register'
-import { Configuration } from '@midwayjs/decorator'
-
-
-@Configuration({
-  imports: [require('../../../../src')],
-})
-export class AutoConfiguration {
-}
