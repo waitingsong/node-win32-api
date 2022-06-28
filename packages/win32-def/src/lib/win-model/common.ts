eb7f59f47f5bf9ac724168b8afa430a5bbd5166e
waiting <1661926154@qq.com>
2022-06-28 16:07:29 +0800

    chore(win32-def): update commons

--- packages/win32-def/src/lib/win-model/common.ts
+++ packages/win32-def/src/lib/win-model/common.ts
@@ -1,5 +1,7 @@
 /*  ---------- data types for TypeScript ----------- */
 // https://docs.microsoft.com/en-us/windows/win32/winprog/windows-data-types
+// https://docs.microsoft.com/en-us/windows/win32/winprog64/the-new-data-types
+// https://docs.microsoft.com/en-us/windows/win32/intl/windows-data-types-for-strings
 
 import { BigIntStr } from '@waiting/shared-types'
 
