3dab77c2a8222e2799a8bfab9b8c0cd071ea5f3e
waiting <1661926154@qq.com>
2022-07-07 15:14:30 +0800

    test(boilerplate): export const CI

--- packages/demo/test/root.config.ts
+++ packages/demo/test/root.config.ts
@@ -1,6 +1,6 @@
 import { genCurrentDirname } from '@waiting/shared-core'
 
 
-const CI = !! process.env.CI
+export const CI = !! process.env.CI
 export const testBaseDir = genCurrentDirname(import.meta.url)
 
