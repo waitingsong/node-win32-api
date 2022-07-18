2cdb74c6278aca404bcdd67c92b4427f04dec134
waiting <1661926154@qq.com>
2022-07-18 17:41:55 +0800

    test(win32-api): update test/user32/62.GetCaretPos.test.ts

--- packages/win32-api/test/user32/62.GetCaretPos.test.ts
+++ packages/win32-api/test/user32/62.GetCaretPos.test.ts
@@ -12,28 +12,17 @@ import {
   DStruct as DS,
   StructFactory,
 } from '../../src/index.js'
-import { calcLpszClassNotepad } from '../config.unittest.js'
-import { user32, destroyWin } from '../helper.js'
+import { user32 } from '../helper.js'
 
 
 describe(fileShortPath(import.meta.url), () => {
 
-  describe('Should FlashWindow() work', () => {
+  describe('Should GetCaretPos() work', () => {
     it('true', async () => {
-      const child = spawn('notepad.exe')
-      await sleep(1000)
-
-      const hWnd = await user32.FindWindowExW(0, 0, calcLpszClassNotepad, null)
-      assert((typeof hWnd === 'string' && hWnd.length > 0) || hWnd > 0)
-
       const point = StructFactory<M.POINT>(DS.POINT)
       const ret = await user32.GetCaretPos(point.ref())
       assert(ret > 0)
       console.log({ x: point.x, y: point.y })
-
-      // await sleep(1000)
-      await destroyWin(hWnd)
-      child.kill()
     })
   })
 })
