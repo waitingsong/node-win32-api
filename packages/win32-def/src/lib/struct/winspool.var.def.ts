2f4040cd7b48e81be0c79c6389cb26e8397d82e2
waiting <1661926154@qq.com>
2022-07-19 17:52:38 +0800

    feat(win32-def): def LPRINTER_DEFAULTS

--- packages/win32-def/src/lib/struct/winspool.var.def.ts
+++ packages/win32-def/src/lib/struct/winspool.var.def.ts
@@ -5,4 +5,5 @@ import { Def } from '../def.enum.js'
  * @link https://docs.microsoft.com/zh-cn/windows/win32/printdocs/printer-defaults
  */
 export const PRINTER_DEFAULTS = Def.ptr
+export const LPRINTER_DEFAULTS = Def.ptr
 
