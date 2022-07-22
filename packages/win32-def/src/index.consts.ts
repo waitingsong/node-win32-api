097fc3922ecb2716aebbe63f3e36d8460f9048a3
waiting <1661926154@qq.com>
2022-07-22 14:08:43 +0800

    feat(win32-def): enum PrinterEnumFlags

--- packages/win32-def/src/index.consts.ts
+++ packages/win32-def/src/index.consts.ts
@@ -1,5 +1,6 @@
 
 export * from './lib/consts/wingdi.enum.js'
+export * from './lib/consts/winspool.enum.js'
 export * from './lib/consts/winuser.const.js'
 export * from './lib/keyboard-mouse-input/virtual-key.consts.js'
 
