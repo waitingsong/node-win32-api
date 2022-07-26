c0c3a039d67a97256caf0063616ce532b07724fa
waiting <1661926154@qq.com>
2022-07-26 13:27:44 +0800

    ci: 1

--- packages/win32-def/rollup.config.js
+++ packages/win32-def/rollup.config.js
@@ -1,7 +1,7 @@
-import { dirname, basename } from 'path'
+import { basename } from 'node:path'
 // import commonjs from '@rollup/plugin-commonjs'
 // import resolve from '@rollup/plugin-node-resolve'
-import { terser } from 'rollup-plugin-terser'
+// import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
 // `npm run build` -> `production` is true
@@ -18,7 +18,6 @@ if (name.slice(0, 1) === '@') {
 name = parseName(name)
 console.log({ name })
 
-const targetDir = dirname(pkg.main)
 const deps = pkg.dependencies
 const peerDeps = pkg.peerDependencies
 
@@ -157,7 +156,8 @@ if (production) {
 */
 
 if (pkg.bin) {
-  const shebang = `#!/usr/bin/env node\n\n${banner}`
+  // const shebang = `#!/usr/bin/env node\n\n${banner}`
+  const shebang = `#!/usr/bin/env ts-node-esm\n\n${banner}`
 
   for (const binPath of Object.values(pkg.bin)) {
     if (! binPath) {
@@ -174,7 +174,8 @@ if (pkg.bin) {
         {
           file: binPath,
           banner: shebang,
-          format: 'cjs',
+          // format: 'cjs',
+          format: 'esm',
           globals,
         },
       ],
