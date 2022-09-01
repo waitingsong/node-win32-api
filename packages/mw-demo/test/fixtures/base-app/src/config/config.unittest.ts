33676a91fa84d18b39ae4afb5c4910e4c1f518ad
waiting <1661926154@qq.com>
2022-09-01 15:47:46 +0800

    chore(boilerplate): tsconfig.json

--- packages/mw-demo/test/fixtures/base-app/src/config/config.unittest.ts
+++ packages/mw-demo/test/fixtures/base-app/src/config/config.unittest.ts
@@ -2,7 +2,7 @@ import { Config, MiddlewareConfig } from '~/lib/types'
 
 
 export const demoConfig: Config = {
-  enableHelloRoute: true,
+  enableDefaultRoute: true,
 }
 
 export const demoMiddlewareConfig: Readonly<Omit<MiddlewareConfig, 'match'>> = {
