fbee615a6ce9758ea11fe65bb24dd608a45efc74
waiting <1661926154@qq.com>
2023-01-31 13:25:14 +0800

    ci: update init-repo.sh

--- .githooks/init-repo.sh
+++ .githooks/init-repo.sh
@@ -9,18 +9,21 @@ set -e
 
 git init
 git config --global i18n.commitencoding utf-8
-git config --local core.autocrlf false
-git config --local core.eol lf
-git config --local core.hooksPath ./.githooks
-git config --local core.ignorecase false
-git config --local core.precomposeUnicode true
-git config --local fetch.prune true
-git config --local pull.rebase true
-git config --local push.followTags true
-git config --local rebase.autoStash true
-git config --local remote.origin.prune true
-git config --local remote.origin.tagopt --tags
-git config --local remote.pushdefault origin
+
+if [ -z "$CI" ]; then
+  git config --local core.autocrlf false
+  git config --local core.eol lf
+  git config --local core.hooksPath ./.githooks
+  git config --local core.ignorecase false
+  git config --local core.precomposeUnicode true
+  git config --local fetch.prune true
+  git config --local pull.rebase true
+  git config --local push.followTags true
+  git config --local rebase.autoStash true
+  git config --local remote.origin.prune true
+  git config --local remote.origin.tagopt --tags
+  git config --local remote.pushdefault origin
+fi;
 
 if [ -z $CI ]; then
   echo It may going for a long time. Plese wait...
