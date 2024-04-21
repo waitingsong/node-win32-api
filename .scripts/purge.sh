5a3a54a20b3b14b45fe9ecd694bf7a1422db1f84
waiting <1661926154@qq.com>
2024-04-21 22:28:15 +0800

    ci: update script

--- .scripts/purge.sh
+++ .scripts/purge.sh
@@ -2,9 +2,15 @@
 
 
 npm run clean
+
 if [[ -d node_modules/nx ]]; then
   nx reset
 fi
+
 rm -rf node_modules package-lock.json
 npm run refresh
+
+if [[ -d node_modules/nx ]]; then
+  nx reset
+fi
 
