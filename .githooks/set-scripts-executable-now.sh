0e64e83edf38f8273c17e12c26852c0a437522b2
waiting <1661926154@qq.com>
2024-05-13 18:42:40 +0800

    ci: update scripts

---
+++ .githooks/set-scripts-executable-now.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set +e
+
+dirs=".scripts .github src bin"
+
+for dir in $dirs; do
+  find $dir -type f \( -iname "*.sh" -o -iname "*.mjs" -o -iname "*.mts" \) -print0 | xargs -0II chmod a+x I
+done
+
+set -e
+
--- .githooks/set-scripts-executable.sh
+++ .githooks/set-scripts-executable.sh
@@ -4,9 +4,7 @@ set +e
 dirs=".scripts .github"
 
 for dir in $dirs; do
-  find $dir -type f -iname "*.sh" -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
-  find $dir -type f -iname "*.mjs" -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
-  find $dir -type f -iname "*.mts" -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
+  find $dir -type f \( -iname "*.sh" -o -iname "*.mjs" -o -iname "*.mts" \) -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
 done
 
 find .githooks -type f -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
