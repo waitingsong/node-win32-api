65e10fb3fc98d9d8f4d2bb1e6807f00d209c9dc5
waiting <1661926154@qq.com>
2024-05-18 17:48:06 +0800

    chore: update scripts

--- .githooks/set-scripts-executable-now.sh
+++ .githooks/set-scripts-executable-now.sh
@@ -4,7 +4,7 @@ set +e
 dirs=".scripts .github src bin"
 
 for dir in $dirs; do
-  find $dir -type f \( -iname "*.sh" -o -iname "*.mjs" -o -iname "*.mts" \) -print0 | xargs -0II chmod a+x I
+  [[ -f "$dir" ]] && find $dir -type f \( -iname "*.sh" -o -iname "*.mjs" -o -iname "*.mts" \) -print0 | xargs -0II chmod a+x I
 done
 
 set -e
--- .githooks/set-scripts-executable.sh
+++ .githooks/set-scripts-executable.sh
@@ -4,7 +4,7 @@ set +e
 dirs=".scripts .github"
 
 for dir in $dirs; do
-  find $dir -type f \( -iname "*.sh" -o -iname "*.mjs" -o -iname "*.mts" \) -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
+  [[ -f "$dir" ]] && find $dir -type f \( -iname "*.sh" -o -iname "*.mjs" -o -iname "*.mts" \) -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
 done
 
 find .githooks -type f -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
