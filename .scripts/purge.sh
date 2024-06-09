d28acddd6ff6dbf518e530474cb131032fb651c6
waiting <1661926154@qq.com>
2024-06-09 21:48:47 +0800

    ci: update .gitignore and purge.sh

--- .scripts/purge.sh
+++ .scripts/purge.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+set +e
 
-
-npm run clean
 
 if [[ -d node_modules/nx ]]; then
   nx reset
 fi
 
+rm -rf .nx
 rm -rf node_modules package-lock.json
+npm i
 npm run refresh
 
 if [[ -d node_modules/nx ]]; then
   nx reset
 fi
+
+# rm -rf .nx
+# workaround for lerna Error: LOCK-FILES-CHANGED
+npm run build
 
