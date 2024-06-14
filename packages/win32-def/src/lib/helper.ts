43bcd8fe4c129e8a2b8abb29dc7785b001a3bd96
waiting <1661926154@qq.com>
2024-06-14 10:54:42 +0800

    chore: lint ignore

--- packages/win32-def/src/lib/helper.ts
+++ packages/win32-def/src/lib/helper.ts
@@ -42,6 +42,7 @@ export function StructType<T extends StructDefType>(
 }
 export function StructFactory<T>(input: StructDefType, options?: StructCharOptions): T {
   // @ts-ignore
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   return (new genInitTyp(input, options)()) as unknown as T
 }
 
