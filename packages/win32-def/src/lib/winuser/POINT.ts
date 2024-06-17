268805a32eb5c3ce15e55bd09013c9bdf38d0e83
waiting <1661926154@qq.com>
2024-06-17 17:38:39 +0800

    ci: up

---
+++ packages/win32-def/src/lib/winuser/POINT.ts
@@ -0,0 +1,33 @@
+import * as W from '../common.def.js'
+import * as M from '../common.types.js'
+import { KoffiTypeResult, genSimpleStruct } from '../helper2.js'
+
+
+const init = {
+  x: W.LONG,
+  y: W.LONG,
+} as const
+const key = 'POINT'
+const ptr = `${key}*`
+
+
+/**
+ * POINT structure
+ * @link https://learn.microsoft.com/en-us/windows/win32/api/windef/ns-windef-point
+ */
+export function POINT_Factory(): KoffiTypeResult {
+  return genSimpleStruct(init, key, ptr)
+}
+
+/**
+ * POINT structure
+ * @link https://learn.microsoft.com/en-us/windows/win32/api/windef/ns-windef-point
+ */
+export interface POINT_Type {
+  x: M.LONG
+  y: M.LONG
+}
+
+export const LPPOINT = ptr
+export const POINT_Init = init
+
--- packages/win32-def/src/lib/winuser/winuser.index.ts
+++ packages/win32-def/src/lib/winuser/winuser.index.ts
@@ -3,3 +3,5 @@ export * from './HARDWAREINPUT.js'
 export * from './INPUT.js'
 export * from './KEYBDINPUT.js'
 export * from './MOUSEINPUT.js'
+export * from './POINT.js'
+
