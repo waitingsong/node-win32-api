25f4cdc9e82bc9d340e7dbafb2b8640495d5b055
waiting <1661926154@qq.com>
2024-06-17 18:51:48 +0800

    ci: up

--- packages/win32-def/src/index.struct.ts
+++ packages/win32-def/src/index.struct.ts
@@ -1,4 +1,5 @@
 
+export * from './lib/comctl/comctl.index.js'
 export * from './lib/windef/windef.index.js'
 export * from './lib/winuser/winuser.index.js'
 
--- packages/win32-def/src/lib/winuser/winuser.index.ts
+++ packages/win32-def/src/lib/winuser/winuser.index.ts
@@ -2,7 +2,6 @@
 export * from './ALTTABINFO.js'
 export * from './COPYDATASTRUCT.js'
 export * from './HARDWAREINPUT.js'
-export * from './INITCOMMONCONTROLSEX.js'
 export * from './INPUT.js'
 export * from './KEYBDINPUT.js'
 export * from './MOUSEINPUT.js'
