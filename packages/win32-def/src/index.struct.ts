a1baa1de4c05900e16ca689ce83f4e053b47679f
waiting <1661926154@qq.com>
2024-06-17 21:45:41 +0800

    ci: up

--- packages/win32-def/src/index.struct.ts
+++ packages/win32-def/src/index.struct.ts
@@ -2,5 +2,6 @@
 export * from './lib/comctl/comctl.index.js'
 export * from './lib/minwinbase/minwinbase.index.js'
 export * from './lib/windef/windef.index.js'
+export * from './lib/wingdi/wingdi.index.js'
 export * from './lib/winuser/winuser.index.js'
 
