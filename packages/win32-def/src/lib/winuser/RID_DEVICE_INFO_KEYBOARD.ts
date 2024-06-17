ed7f5be495da8d98f537c62c9aa69fba3d3fd761
waiting <1661926154@qq.com>
2024-06-17 21:16:40 +0800

    ci: RID_DEVICE_INFO_KEYBOARD.ts

---
+++ packages/win32-def/src/lib/winuser/RID_DEVICE_INFO_KEYBOARD.ts
@@ -0,0 +1,41 @@
+import * as W from '../common.def.js'
+import * as M from '../common.types.js'
+import { genStruct, type KoffiTypeResult } from '../helper2.js'
+
+
+const key = 'RID_DEVICE_INFO_KEYBOARD'
+const ptr = `${key}*`
+const init = {
+  dwType: W.DWORD,
+  dwSubType: W.DWORD,
+  dwKeyboardMode: W.DWORD,
+  dwNumberOfFunctionKeys: W.DWORD,
+  dwNumberOfIndicators: W.DWORD,
+  dwNumberOfKeysTotal: W.DWORD,
+} as const
+
+
+/**
+ * RID_DEVICE_INFO_KEYBOARD structure
+ * @link https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-RID_DEVICE_INFO_KEYBOARD
+ */
+export function RID_DEVICE_INFO_KEYBOARD_Factory(): KoffiTypeResult {
+  return genStruct(init, key, ptr)
+}
+
+/**
+ * RID_DEVICE_INFO_KEYBOARD structure
+ * @link https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-RID_DEVICE_INFO_KEYBOARD
+ */
+export interface RID_DEVICE_INFO_KEYBOARD_Type {
+  dwType: M.DWORD
+  dwSubType: M.DWORD
+  dwKeyboardMode: M.DWORD
+  dwNumberOfFunctionKeys: M.DWORD
+  dwNumberOfIndicators: M.DWORD
+  dwNumberOfKeysTotal: M.DWORD
+}
+
+export const PRID_DEVICE_INFO_KEYBOARD = ptr
+export const RID_DEVICE_INFO_KEYBOARD_Init = init
+
---
+++ packages/win32-def/test/winuser/118.RID_DEVICE_INFO_KEYBOARD.test.ts
@@ -0,0 +1,22 @@
+import assert from 'node:assert/strict'
+
+import { fileShortPath } from '@waiting/shared-core'
+
+import { RID_DEVICE_INFO_KEYBOARD_Factory, PRID_DEVICE_INFO_KEYBOARD } from '##/index.struct.js'
+import { assertStructUnion } from '#@/helper.js'
+
+
+const name = 'RID_DEVICE_INFO_KEYBOARD'
+const pointer = PRID_DEVICE_INFO_KEYBOARD
+const factory = RID_DEVICE_INFO_KEYBOARD_Factory
+const fn = `${name}_Factory`
+
+describe(fileShortPath(import.meta.url), () => {
+  describe(fn, () => {
+    it('normal', () => {
+      const data = factory()
+      assertStructUnion(data, { name, pointer, size: 24 })
+    })
+  })
+})
+
