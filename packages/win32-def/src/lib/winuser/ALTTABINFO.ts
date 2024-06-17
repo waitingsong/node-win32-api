85bb79c9b9c9a78716aebd592a663392a769e74b
waiting <1661926154@qq.com>
2024-06-17 18:09:59 +0800

    ci: 106.ALTTABINFO.test.ts

---
+++ packages/win32-def/src/lib/winuser/ALTTABINFO.ts
@@ -0,0 +1,48 @@
+import * as W from '../common.def.js'
+import * as M from '../common.types.js'
+import { KoffiTypeResult, genStruct } from '../helper2.js'
+
+import { POINT_Factory, type POINT_Type } from './POINT.js'
+
+
+const key = 'ALTTABINFO'
+const ptr = `${key}*`
+const init = {
+  cbSize: W.DWORD,
+  cItems: W.INT,
+  cColumns: W.INT,
+  cRows: W.INT,
+  iColFocus: W.INT,
+  iRowFocus: W.INT,
+  cxItem: W.INT,
+  cyItem: W.INT,
+  ptStart: POINT_Factory,
+} as const
+
+/**
+ * ALTTABINFO structure
+ * @link https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-alttabinfo
+ */
+export function ALTTABINFO_Factory(): KoffiTypeResult {
+  return genStruct(init, key, ptr)
+}
+
+/**
+ *  ALTTABINFO structure
+ * @link https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-alttabinfo
+ */
+export interface ALTTABINFO_Type {
+  cbSize: M.DWORD
+  cItems: M.INT
+  cColumns: M.INT
+  cRows: M.INT
+  iColFocus: M.INT
+  iRowFocus: M.INT
+  cxItem: M.INT
+  cyItem: M.INT
+  ptStart: POINT_Type
+}
+
+export const LPALTTABINFO = ptr
+export const ALTTABINFO_Init = init
+
---
+++ packages/win32-def/test/winuser/106.ALTTABINFO.test.ts
@@ -0,0 +1,27 @@
+import assert from 'node:assert/strict'
+
+import { fileShortPath } from '@waiting/shared-core'
+
+import { ALTTABINFO_Factory, LPALTTABINFO } from '##/lib/winuser/winuser.index.js'
+import { assertStructUnion } from '#@/helper.js'
+
+
+const fn = 'ALTTABINFO_Factory'
+const factory = ALTTABINFO_Factory
+const name = 'ALTTABINFO'
+const pointer = LPALTTABINFO
+
+describe(fileShortPath(import.meta.url), () => {
+  describe(fn, () => {
+    it('normal', () => {
+      const data = factory()
+      assertStructUnion(data, { name, pointer, size: 40 })
+    })
+
+    it('point cache', () => {
+      const data = factory()
+      assertStructUnion(data, { name, pointer, size: 40 })
+    })
+  })
+})
+
