7ea243a93754100ee61aad0bb149914544c203f7
waiting <1661926154@qq.com>
2024-06-19 21:50:21 +0800

    ci: up

--- packages/win32-api/test/foo2.ts
+++ packages/win32-api/test/foo2.ts
@@ -6,17 +6,18 @@ const user32 = koffi.load('user32.dll')
 // const FindWindowEx = user32.func('FindWindowExW', 'int', ['int', 'int', 'str16', 'str16'])
 const FindWindowEx = user32.func('FindWindowExW', 'int', ['int', 'int', 'int16*', 'int16*'])
 const GetWindowThreadProcessId = user32.func('GetWindowThreadProcessId', 'int', ['int', 'int16*'])
-const GetWindowTextW = user32.func('GetWindowTextW', 'int', ['int', 'int16*', 'int'])
+const GetWindowTextW = user32.func('GetWindowTextW', 'int', ['int', 'uint16*', 'int'])
+// const GetWindowTextW = user32.func('GetWindowTextW', 'int', ['int', 'str16', 'int'])
 
 for (let hwnd = 0; ;) {
   // hwnd = FindWindowEx(0, hwnd, 'Chrome_WidgetWin_1', null)
   // hwnd = FindWindowEx(0, hwnd, 'Notepad', null)
   // hwnd = FindWindowEx(0, hwnd, Buffer.from('Notepad', 'ucs2'), null)
   // hwnd = FindWindowEx(0, hwnd, Buffer.from('Notepad', 'ucs2'), '无标题 - 记事本')
-  hwnd = FindWindowEx(0, hwnd, Buffer.from('Notepad', 'ucs2'), Buffer.from('无标题 - 记事本', 'ucs2'))
+  // hwnd = FindWindowEx(0, hwnd, Buffer.from('Notepad', 'ucs2'), Buffer.from('无标题 - 记事本', 'ucs2'))
   // hwnd = FindWindowEx(0, hwnd, 'Notepad', '无标题 - 记事本')
   // hwnd = FindWindowEx(0, hwnd, 'ApplicationFrameWindow', null)
-  // hwnd = FindWindowEx(0, hwnd, 'ApplicationFrameWindow', '计算器')
+  hwnd = FindWindowEx(0, hwnd, 'ApplicationFrameWindow', '计算器')
   // hwnd = FindWindowEx(0, hwnd, null, '计算器')
 
   // hwnd = await new Promise((resolve, reject) => {
