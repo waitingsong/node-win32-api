2e8a019b16e1d107a3024d2af4e66e4cb9e9fd75
waiting <1661926154@qq.com>
2024-06-19 15:23:09 +0800

    ci: up

---
+++ packages/win32-def/src/lib/winspool/helper.type.ts
@@ -0,0 +1,17 @@
+import { PRINTER_INFO_1_Type } from './PRINTER_INFO_1.js'
+import { PRINTER_INFO_4_Type } from './PRINTER_INFO_4.js'
+
+
+export * from './DOC_INFO_1.js'
+export * from './PRINTER_DEFAULTS.js'
+export * from './PRINTER_INFO_1.js'
+export * from './PRINTER_INFO_4.js'
+export * from './PRINTPROCESSOR_INFO_1.js'
+
+
+export type PRINTER_INFO_X_Type<X extends number> = X extends 1
+  ? PRINTER_INFO_1_Type
+  : X extends 4
+    ? PRINTER_INFO_4_Type
+    : never
+
--- packages/win32-def/src/lib/winspool/winspool.index.ts
+++ packages/win32-def/src/lib/winspool/winspool.index.ts
@@ -5,3 +5,5 @@ export * from './PRINTER_INFO_1.js'
 export * from './PRINTER_INFO_4.js'
 export * from './PRINTPROCESSOR_INFO_1.js'
 
+export * from './helper.type.js'
+
