33e15035c507f88a350439211cf10fd20c4807ab
waiting <1661926154@qq.com>
2024-06-19 16:14:24 +0800

    test: up

--- packages/win32-api/test/lib/21.main.test.ts
+++ packages/win32-api/test/lib/21.main.test.ts
@@ -3,18 +3,16 @@ import fs from 'node:fs'
 import { join, normalize } from 'node:path'
 
 import { fileShortPath, genCurrentDirname } from '@waiting/shared-core'
+import * as T from 'win32-def'
+import * as D from 'win32-def/def'
+import * as S from 'win32-def/struct'
 
-import {
-  Types as M,
-  DTypes as W,
-  DStruct as DS,
-} from '##/index.js'
 import * as Win from '##/index.js'
 
 
 const __dirname = genCurrentDirname(import.meta.url)
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-const dllDir = normalize(join(__dirname, '/../src/lib/'))
+const dllDir = normalize(join(__dirname, '../../src/lib/'))
 const dlls: string[] = []
 
 for (const key of fs.readdirSync(dllDir)) {
@@ -36,11 +34,11 @@ describe(fileShortPath(import.meta.url), () => {
 
     describe(apiName, () => {
 
-      it('normal', async () => {
+      it.only('normal', async () => {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         if (module?.apiDef) {
           // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-          const api = module.load() as M.DllFuncs
+          const api = module.load() as T.DllFuncs
 
           for (const fn in api) {
             if (! {}.hasOwnProperty.call(api, fn)) {
@@ -52,7 +50,7 @@ describe(fileShortPath(import.meta.url), () => {
           }
         }
         else {
-          assert(false, 'module or module.apiDef invalie')
+          assert(false, 'module or module.apiDef invalid')
         }
       })
     })
