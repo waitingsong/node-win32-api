9767741089d4eef64524227738c22792f87390c6
waiting <1661926154@qq.com>
2024-06-19 15:25:39 +0800

    ci: up

--- packages/win32-def/src/lib/winspool/helper.type.ts
+++ packages/win32-def/src/lib/winspool/helper.type.ts
@@ -1,3 +1,5 @@
+import { EnumPrinters_Level } from '../common.types.js'
+
 import { PRINTER_INFO_1_Type } from './PRINTER_INFO_1.js'
 import { PRINTER_INFO_4_Type } from './PRINTER_INFO_4.js'
 
@@ -9,7 +11,7 @@ export * from './PRINTER_INFO_4.js'
 export * from './PRINTPROCESSOR_INFO_1.js'
 
 
-export type PRINTER_INFO_X_Type<X extends number> = X extends 1
+export type PRINTER_INFO_X_Type<X extends EnumPrinters_Level> = X extends 1
   ? PRINTER_INFO_1_Type
   : X extends 4
     ? PRINTER_INFO_4_Type
