661008e0069734b226fb917d78450f09f4595145
waiting <1661926154@qq.com>
2024-06-19 15:37:55 +0800

    ci: up

--- packages/win32-api/src/lib/comctl32/api.types.ts
+++ packages/win32-api/src/lib/comctl32/api.types.ts
@@ -1,7 +1,8 @@
 import * as T from 'win32-def'
+import * as S from 'win32-def/struct'
 
 
 export interface Win32Fns {
-  InitCommonControlsEx: (lpInitCtrls: T.LPINITCOMMONCONTROLSEX) => T.BOOL
+  InitCommonControlsEx: (lpInitCtrls: S.INITCOMMONCONTROLSEX_Type) => T.BOOL
 }
 
