f83466d0f4fa0f22ed4e42bc09ca397f043ae726
waiting <1661926154@qq.com>
2024-06-19 21:46:59 +0800

    ci: clean

--- packages/win32-api/src/lib/types.ts
+++ packages/win32-api/src/lib/types.ts
@@ -16,28 +16,4 @@ export const enum DllNames {
 }
 
 
-export interface RegisterFunctionOpts {
-  /**
-   * DLL library,
-   * lib = koffi.load('user32.dll')
-   */
-  lib: IKoffiLib
-  /** function name */
-  name: string
-  /** function parameters */
-  params: FnParams
-  /**
-   * Calling convention
-   * @default 'Stdcall' (for Windows)
-   * @link https://koffi.dev/functions#calling-conventions
-   */
-  convention?: CallingConvention
-}
-
-export interface LoadOptions<T = unknown> {
-  dll: string
-  dllFuncs: DllFuncs<T>
-  usedFuncNames?: FnName[]
-  settings?: LoadSettings
-}
 
