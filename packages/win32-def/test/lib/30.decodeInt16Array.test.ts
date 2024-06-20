b83cb27408c2a72dc90f6fbd29503403dcf0424c
waiting <1661926154@qq.com>
2024-06-20 12:45:35 +0800

    ci: up

---
+++ packages/win32-def/test/lib/30.decodeInt16Array.test.ts
@@ -0,0 +1,50 @@
+import assert from 'node:assert'
+import { spawn } from 'node:child_process'
+
+import { fileShortPath, sleep } from '@waiting/shared-core'
+import ffi from 'koffi'
+
+import * as D from '##/index.def.js'
+import { LoadOptions, decodeInt16Array } from '##/index.js'
+import * as T from '##/index.js'
+import { DISPLAY_DEVICEW_Factory } from '##/index.struct.js'
+import { load } from '##/lib/loader/loader.js'
+
+import { type Win32Fns, apiDef } from './21a.helper.js'
+
+
+describe(fileShortPath(import.meta.url), () => {
+  const options: LoadOptions<Win32Fns> = {
+    dll: 'user32.dll',
+    dllFuncs: apiDef,
+  }
+
+  describe('decodeInt16Array()', () => {
+    it('normal', async () => {
+      const { size, payload } = DISPLAY_DEVICEW_Factory()
+      payload.cb = size
+
+      const lib = load<Win32Fns>(options)
+
+      const child = spawn('calc.exe')
+      await sleep(1500)
+
+      const res = await lib.EnumDisplayDevicesWAsync(null, 0, payload, 1)
+
+      const DeviceID = decodeInt16Array(payload.DeviceID)
+      const DeviceKey = decodeInt16Array(payload.DeviceKey)
+      const DeviceName = decodeInt16Array(payload.DeviceName)
+      const DeviceString = decodeInt16Array(payload.DeviceString)
+
+      console.info({ res, DeviceID, DeviceKey, DeviceName, DeviceString })
+
+      assert(DeviceID.startsWith('PCI\\VEN_') || DeviceID.includes('VMBUS') || DeviceID === '', DeviceID)
+      assert(typeof DeviceKey === 'string', DeviceKey)
+      assert(DeviceName === '\\\\.\\DISPLAY1', DeviceName)
+      assert(DeviceString.length > 0)
+      const flag = ['Microsoft Hyper-V', 'Intel', 'AMD', 'Radeon'].some(val => DeviceString.includes(val))
+      assert(flag, DeviceString)
+    })
+  })
+})
+
