506d61b6e12caf5a8c8b00ab288514c19da6f970
waiting <1661926154@qq.com>
2024-06-20 12:32:08 +0800

    ci: up

--- packages/win32-def/src/index.struct.ts
+++ packages/win32-def/src/index.struct.ts
@@ -1,8 +1,3 @@
 
-export * from './lib/comctl/comctl.index.js'
-export * from './lib/minwinbase/minwinbase.index.js'
-export * from './lib/windef/windef.index.js'
-export * from './lib/wingdi/wingdi.index.js'
-export * from './lib/winspool/winspool.index.js'
-export * from './lib/winuser/winuser.index.js'
+export * from './lib/struct/struct.index.js'
 
--- packages/win32-def/src/index.ts
+++ packages/win32-def/src/index.ts
@@ -10,7 +10,7 @@ export * from './lib/config.js'
 export * from './lib/def.enum.js'
 
 export * from './lib/common.types.js'
-export * from './lib/helper2.js'
+export * from './lib/struct.helper.js'
 export * from './lib/ffi.types.js'
 export * from './lib/types.js'
 export * from './lib/util.js'
