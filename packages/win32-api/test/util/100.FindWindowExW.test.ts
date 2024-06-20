e5d7369a5a9f3e2f60ad1dccdc48b3c8a625c454
waiting <1661926154@qq.com>
2024-06-20 17:32:19 +0800

    ci: up

--- packages/win32-api/test/util/100.FindWindowExW.test.ts
+++ packages/win32-api/test/util/100.FindWindowExW.test.ts
@@ -8,13 +8,17 @@ import { FindWindowExW } from '##/index.util.js'
 
 describe(fileShortPath(import.meta.url), () => {
   describe('FindWindowExW()', () => {
-    it('find Notepad', async () => {
+    it('find Notepad hwnd', async () => {
       const child = spawn('notepad.exe')
       try {
         await sleep(1500)
         const hwnd = await FindWindowExW(0, 0, 'Notepad', null)
         assert(typeof hwnd === 'number' || typeof hwnd === 'bigint')
         assert(hwnd)
+
+        const hwnd2 = await FindWindowExW(0, 0, 'Notepad', null)
+        assert(typeof hwnd2 === 'number' || typeof hwnd2 === 'bigint')
+        assert(hwnd2)
       }
       finally {
         child.kill()
