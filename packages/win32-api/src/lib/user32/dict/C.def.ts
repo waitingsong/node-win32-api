4171d6db14e731aa9b3e913e72878895c3d3e76a
waiting <1661926154@qq.com>
2024-06-28 12:47:15 +0800

    ci: up

--- packages/win32-api/src/lib/user32/dict/C.def.ts
+++ packages/win32-api/src/lib/user32/dict/C.def.ts
@@ -31,3 +31,15 @@ export class DefUser32_C extends DefUser32_B {
 
 }
 
+
+export class DefUser32_D extends DefUser32_C {
+
+  static DefWindowProcW = [D.LRESULT, [D.HWND, D.UINT, D.WPARAM, D.LPARAM]]
+
+  /** https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-destroywindow */
+  static DestroyWindow = [D.BOOL, [D.HWND]]
+
+  /** https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-dispatchmessagew */
+  static DispatchMessageW = [D.LRESULT, [S.LPMSG]]
+
+}
--- packages/win32-api/src/lib/user32/dict/C.types.ts
+++ packages/win32-api/src/lib/user32/dict/C.types.ts
@@ -29,4 +29,16 @@ export class User32_C extends User32_B {
 
 }
 
+export class User32_D extends User32_C {
+
+  DefWindowProcW: (hWnd: T.HWND, Msg: T.UINT, wParam: T.WPARAM, lParam: T.LPARAM) => T.LRESULT
+
+  /** https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-destroywindow */
+  DestroyWindow: (hWnd: T.HWND) => T.BOOL
+
+  /** https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-dispatchmessagew */
+  DispatchMessageW: (lpMsg: T.LPMSG) => T.LRESULT
+
+}
+
 /* c8 ignore stop */
