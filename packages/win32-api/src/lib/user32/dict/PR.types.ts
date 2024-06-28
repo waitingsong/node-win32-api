5377c6c7b9c85a570f98467aafdb0de4e73ef4c0
waiting <1661926154@qq.com>
2024-06-28 15:59:43 +0800

    chore: lint fix

--- packages/win32-api/src/lib/user32/dict/PR.types.ts
+++ packages/win32-api/src/lib/user32/dict/PR.types.ts
@@ -1,6 +1,6 @@
 /* c8 ignore start */
-import * as T from 'win32-def/types'
 import * as S from 'win32-def/struct'
+import * as T from 'win32-def/types'
 
 import { User32_M } from './IM.types.js'
 
