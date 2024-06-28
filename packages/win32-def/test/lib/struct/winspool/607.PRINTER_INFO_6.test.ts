324b2ecedcb00f64e0575145e79ac4719a365f01
waiting <1661926154@qq.com>
2024-06-28 12:14:30 +0800

    ci: up

---
+++ packages/win32-def/test/lib/struct/winspool/607.PRINTER_INFO_6.test.ts
@@ -0,0 +1,21 @@
+import { fileShortPath } from '@waiting/shared-core'
+
+import { PRINTER_INFO_6_Factory, PPRINTER_INFO_6 } from '##/index.struct.js'
+import { assertStructUnion } from '#@/helper.js'
+
+
+const name = 'PRINTER_INFO_6'
+const pointer = PPRINTER_INFO_6
+const factory = PRINTER_INFO_6_Factory
+const size = 32
+const fn = `${name}_Factory`
+
+describe(fileShortPath(import.meta.url), () => {
+  describe(fn, () => {
+    it('normal', () => {
+      const data = factory()
+      assertStructUnion(data, { name, pointer, size })
+    })
+  })
+})
+
