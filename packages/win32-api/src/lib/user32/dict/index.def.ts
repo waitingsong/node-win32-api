e25e1cc81259aea4562fe203218d37bf5814d724
waiting <1661926154@qq.com>
2024-06-28 12:55:45 +0800

    ci: up

--- packages/win32-api/src/lib/user32/dict/index.def.ts
+++ packages/win32-api/src/lib/user32/dict/index.def.ts
@@ -1,3 +1,3 @@
 
-export { DefUser32_U as DefUser32Combo } from './U.def.js'
+export { DefUser32_U as DefUser32Combo } from './TU.def.js'
 
--- packages/win32-api/src/lib/user32/dict/index.types.ts
+++ packages/win32-api/src/lib/user32/dict/index.types.ts
@@ -1,4 +1,4 @@
-import { User32_U } from './U.types.js'
+import { User32_U } from './TU.types.js'
 
 
 export class User32Combo extends User32_U {
