f1cad0a133017f9aad5823a42875ebb0dbf49442
waiting <1661926154@qq.com>
2024-06-28 13:44:49 +0800

    ci: up

--- packages/win32-def/test/mapper/GetPrinterW.mapper.ts
+++ packages/win32-def/test/mapper/GetPrinterW.mapper.ts
@@ -1,28 +1,28 @@
 import assert from 'assert'
 
 import type { FlattenNestedTuple } from '@waiting/shared-types'
+import { getPRINTER_INFO_X_Ptr } from 'win32-def/struct'
+import type { EnumPrinters_Level, MultipleChoiceMapper } from 'win32-def/types'
 
-import { getPRINTER_INFO_X_Ptr } from '##/index.struct.js'
-import type { MultipleChoiceMapper } from '##/index.types.js'
 import { type Winspool, DefWinspool } from '#@/def.class/api.helper.js'
 
 
-export const funcName = 'EnumPrintersW'
+export const funcName = 'GetPrinterW'
 type Func = Winspool[typeof funcName]
 type Params = Parameters<Func>
 
 type DefFunc = typeof DefWinspool[typeof funcName]
 type DefParams = FlattenNestedTuple<DefFunc[1]>
 
-export const EnumPrintersW_mapper: MultipleChoiceMapper<Params, DefParams> = (name, runtimeArgs, defParamsArray) => {
+export const GetPrinterW_mapper: MultipleChoiceMapper<Params, DefParams> = (name, runtimeArgs, defParamsArray) => {
   if (name !== funcName) { return }
-  const argLevel = runtimeArgs[2]
+  const argLevel = runtimeArgs[1] as EnumPrinters_Level
   const argPtr = getPRINTER_INFO_X_Ptr(argLevel)
   assert(argPtr, `getPRINTER_INFO_X_Ptr(${argLevel}) failed`)
 
   for (const row of defParamsArray) {
     assert(Array.isArray(row))
-    const defPtr = row[3] as typeof argPtr
+    const defPtr = row[2] as typeof argPtr
     if (defPtr.endsWith(argPtr)) {
       return row
     }
