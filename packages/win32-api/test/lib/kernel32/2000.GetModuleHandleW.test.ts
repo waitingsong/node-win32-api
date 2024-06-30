45fac64d4362d92e1f6d8d6430d0d671caa8efa6
waiting <1661926154@qq.com>
2024-06-30 18:39:33 +0800

    test(win32-api): add 2000.GetModuleHandleW.test.ts

---
+++ packages/win32-api/test/lib/kernel32/2000.GetModuleHandleW.test.ts
@@ -0,0 +1,31 @@
+import assert from 'node:assert/strict'
+
+import { fileShortPath } from '@waiting/shared-core'
+import { ucsBufferFrom } from 'win32-def'
+
+import { Kernel32 as Lib, DllNames } from '##/index.js'
+
+
+describe(fileShortPath(import.meta.url), () => {
+  describe('GetModuleHandleW()', () => {
+    const name = DllNames.kernel32 + '.dll'
+
+    it('string', () => {
+      const lib = Lib.load()
+      assert(lib)
+
+      const hModule = lib.GetModuleHandleW(name)
+      assert(hModule)
+    })
+
+    it('buffer', () => {
+      const lib = Lib.load()
+      assert(lib)
+
+      const hModule = lib.GetModuleHandleW(ucsBufferFrom(name))
+      assert(hModule)
+    })
+  })
+
+})
+
