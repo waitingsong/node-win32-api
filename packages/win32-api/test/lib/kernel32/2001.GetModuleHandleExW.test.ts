7266706287cad9f4c6f927870c9bb2ba84ed7ea8
waiting <1661926154@qq.com>
2024-07-02 10:45:26 +0800

    test(api): update 2001.GetModuleHandleExW.test.ts

--- packages/win32-api/test/lib/kernel32/2001.GetModuleHandleExW.test.ts
+++ packages/win32-api/test/lib/kernel32/2001.GetModuleHandleExW.test.ts
@@ -3,33 +3,37 @@ import assert from 'node:assert/strict'
 import { fileShortPath } from '@waiting/shared-core'
 import { ucsBufferFrom } from 'win32-def'
 
-import { Kernel32 as Lib, DllNames } from '##/index.js'
+import { Kernel32 as Lib, DllNames, ffi } from '##/index.js'
 
 
 describe(fileShortPath(import.meta.url), () => {
   describe('GetModuleHandleExW()', () => {
     const name = DllNames.kernel32 + '.dll'
 
-    it('string', () => {
+    it.only('string', () => {
       const lib = Lib.load()
       assert(lib)
 
-      const buf = Buffer.alloc(256)
+      const buf = Buffer.alloc(8)
       const ret = lib.GetModuleHandleExW(0, name, buf)
       assert(ret)
       const hModule = buf.readBigUint64LE()
       assert(hModule)
+      const h2 = ffi.decode(buf, 'uintptr') as number
+      assert(h2.toString() === hModule.toString())
     })
 
     it('buffer', () => {
       const lib = Lib.load()
       assert(lib)
 
-      const buf = Buffer.alloc(256)
+      const buf = Buffer.alloc(8)
       const ret = lib.GetModuleHandleExW(0, ucsBufferFrom(name), buf)
       assert(ret)
       const hModule = buf.readBigUint64LE()
       assert(hModule)
+      const h2 = ffi.decode(buf, 'uintptr') as number
+      assert(h2.toString() === hModule.toString())
     })
   })
 
